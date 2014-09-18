be338358dd2328cc2f4fa12274eb928846a59086
GAO <gaodongchen@gmail.com>
2014-09-18 15:57:31 +0800

    update source code

---
+++ application/config/dev/database.ini.php
@@ -0,0 +1,48 @@
+<?php
+return array(
+	'data_map'		=> array(
+		'driver'	=> 'mysql',
+		'host'		=> '192.168.12.41:3308',
+		'login'		=> 'web41',
+		'password'	=> '123465',
+		'database'	=> 'data_map',
+		'charset'	=> 'utf8',
+		'prefix'	=> ''
+	),
+	'data'			=> array(
+		'driver'	=> 'mysql',
+		'host'		=> '192.168.8.111:5510',
+		'login'		=> 'php1_root',
+		'password'	=> 'ZT!hw@478#',
+		'database'	=> 'Data',
+		'charset'	=> 'latin1',
+		'prefix'	=> ''
+	),
+	'matchs'		=> array(
+		'driver'	=> 'mysql',
+		'host'		=> '192.168.8.12:8807',
+		'login'		=> 'datad',
+		'password'	=> 'datapass',
+		'database'	=> 'matchs',
+		'charset'	=> 'utf8',
+		'prefix'	=> ''
+	),
+	'data_basketball'=>array(
+		'driver'    => 'mysql',
+		'host'      => '192.168.8.31:3309',
+		'login'     => 'datad',
+		'password'  => 'datapass',
+		'database'  => 'data_basketball',
+		'charset'   => 'utf8',
+		'prefix'    => ''
+	),
+	'source_betradar'=>array(
+		'driver'    => 'mysql',
+		'host'      => '192.168.12.41:3308',
+		'login'     => 'web41',
+		'password'  => '123465',
+		'database'  => 'tornado_betradar',
+		'charset'   => 'utf8',
+		'prefix'    => ''
+	),
+);
